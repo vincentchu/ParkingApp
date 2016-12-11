fecc7e3b8f561c75289b34edd524c8a3a854c5ed
Vincent Chu <vincentchu@gmail.com>
2016-12-10 16:21:47 -0800

    impl navigation

--- src/ParkingApp.js
+++ src/ParkingApp.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react'
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, NavigatorIOS, TouchableHighlight } from 'react-native';
 import MapView from 'react-native-maps'
 import withCurrentLocation from './with-current-location'
 
@@ -43,10 +43,80 @@ const MapViewBase = (props: { currentLocation: ?Position }) => {
 
 const MapViewWrapped = withCurrentLocation(MapViewBase)
 
+const renderScene = (route, nav) => {
+  console.log('nav', nav)
+  console.log('navigator', navigator)
+
+  const onPress = () => {
+    if (route.index == 0) {
+      nav.push(Routes[1])
+    } else {
+      nav.pop()
+    }
+  }
+
+  return (
+    <TouchableHighlight onPress={onPress}>
+      <Text>Hello, {route.title}</Text>
+    </TouchableHighlight>
+  )
+}
+
+const MyScene = (props) => {
+  const onPress = () => {
+    props.navigator.push(Routes[1])
+  }
+
+  console.log('rendering MyScene')
+  return (
+    <View>
+      <TouchableHighlight onPress={onPress}>
+        <Text>Hello, World - {props.index}</Text>
+      </TouchableHighlight>
+    </View>
+  )
+}
+
+class MyScene2 extends React.Component {
+  props: {
+    title: string,
+    navigator: Object,
+    index: number,
+  }
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  _onForward = () => {
+    this.props.navigator.push(Routes[1]);
+  }
+
+  render() {
+    return (
+      <View style={{ padding: 100 }}>
+        <Text>Current Scene: { this.props.title }</Text>
+        <TouchableHighlight onPress={this._onForward}>
+          <Text>Tap me to load the next scene</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+const Routes = [
+  { title: 'First Scene', component: MyScene2, passProps: { index: 0 } },
+  { title: 'Second Scene', component: MyScene2, passProps: { index: 1 } },
+]
+
+
 export default class ParkingApp extends React.Component {
   render() {
     return (
-      <MapViewWrapped />
+      <NavigatorIOS
+        initialRoute={Routes[0]}
+        style={{flex: 1}}
+      />
     )
   }
 }
