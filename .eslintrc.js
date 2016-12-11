0e43232be064aaa73f1b886ae35e5a8971111e1e
Vincent Chu <vincentchu@gmail.com>
2016-12-11 10:45:12 -0800

    fix quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-  "installedESLint": true,
-  "extends": [
-    "recommended/esnext",
-    "recommended/esnext/style-guide",
-    "recommended/node",
-    "recommended/node/style-guide",
-    "recommended/react-native",
-    "recommended/react-native/style-guide"
+  'installedESLint': true,
+  'extends': [
+    'recommended/esnext',
+    'recommended/esnext/style-guide',
+    'recommended/node',
+    'recommended/node/style-guide',
+    'recommended/react-native',
+    'recommended/react-native/style-guide'
   ],
-  "rules": {
-    "indent": ["error", 2],
-    "react/jsx-indent": [2, 2],
-    "react/jsx-curly-spacing": [2, "never"],
+  'rules': {
+    'indent': ['error', 2],
+    'react/jsx-indent': [2, 2],
+    'react/jsx-curly-spacing': [2, 'never'],
     'react-native/no-inline-styles': 'off',
     'object-property-newline': ['error', {
       allowMultiplePropertiesPerLine: true,
