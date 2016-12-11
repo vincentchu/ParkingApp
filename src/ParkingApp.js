4fd9bd17f7a23c19349c42d000f48c57140464e6
Vincent Chu <vincentchu@gmail.com>
2016-12-10 18:03:56 -0800

    layout coming together, still confused

--- src/ParkingApp.js
+++ src/ParkingApp.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react'
-import { Text, StyleSheet, View, NavigatorIOS, TouchableHighlight } from 'react-native';
+import { Text, StyleSheet, View, Navigator, NavigatorIOS, TouchableHighlight } from 'react-native';
+import { Button, NavigationBar, Title, Row } from '@shoutem/ui'
 import MapView from 'react-native-maps'
 import withCurrentLocation from './with-current-location'
 
@@ -43,80 +44,29 @@ const MapViewBase = (props: { currentLocation: ?Position }) => {
 
 const MapViewWrapped = withCurrentLocation(MapViewBase)
 
+const Routes = [
+  { title: 'Set Location', index: 0 },
+]
+
 const renderScene = (route, nav) => {
-  console.log('nav', nav)
-  console.log('navigator', navigator)
-
-  const onPress = () => {
-    if (route.index == 0) {
-      nav.push(Routes[1])
-    } else {
-      nav.pop()
-    }
-  }
-
   return (
-    <TouchableHighlight onPress={onPress}>
-      <Text>Hello, {route.title}</Text>
-    </TouchableHighlight>
-  )
-}
-
-const MyScene = (props) => {
-  const onPress = () => {
-    props.navigator.push(Routes[1])
-  }
-
-  console.log('rendering MyScene')
-  return (
-    <View>
-      <TouchableHighlight onPress={onPress}>
-        <Text>Hello, World - {props.index}</Text>
-      </TouchableHighlight>
+    <View style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <MapViewWrapped />
+        <NavigationBar styleName="fade" centerComponent={<Title>TITLE</Title>} />
+      </View>
+      <View style={{height: 30}}>
+        <Text>Here</Text>
+      </View>
     </View>
   )
 }
-
-class MyScene2 extends React.Component {
-  props: {
-    title: string,
-    navigator: Object,
-    index: number,
-  }
-
-  constructor(props, context) {
-    super(props, context);
-  }
-
-  _onForward = () => {
-    this.props.navigator.push(Routes[1]);
-  }
-
-  render() {
-    return (
-      <View style={{ padding: 100 }}>
-        <Text>Current Scene: { this.props.title }</Text>
-        <TouchableHighlight onPress={this._onForward}>
-          <Text>Tap me to load the next scene</Text>
-        </TouchableHighlight>
-      </View>
-    )
-  }
-}
-
-const Routes = [
-  { title: 'First Scene', component: MyScene2, passProps: { index: 0 } },
-  { title: 'Second Scene', component: MyScene2, passProps: { index: 1 } },
-]
 
 
 export default class ParkingApp extends React.Component {
   render() {
     return (
-      <NavigatorIOS
-        initialRoute={Routes[0]}
-        style={{flex: 1}}
-      />
+      <Navigator initialRoute={Routes[0]} style={{flex: 1}} renderScene={renderScene} />
     )
   }
 }
