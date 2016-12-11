cf43476fce94adaaf561468a8f9d8f4c693847ef
Vincent Chu <vincentchu@gmail.com>
2016-12-11 11:20:44 -0800

    use redux-persist to have some state from session to session

--- src/redux-store.js
+++ src/redux-store.js
@@ -1,7 +1,12 @@
 // @flow
+import { AsyncStorage } from 'react-native'
 import { createStore, combineReducers } from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import { reducer as mapViewport } from './state/map-viewport'
 
 const reducer = combineReducers({ mapViewport })
 
-export default createStore(reducer)
+const store = createStore(reducer, undefined, autoRehydrate())
+persistStore(store, { storage: AsyncStorage })
+
+export default store
